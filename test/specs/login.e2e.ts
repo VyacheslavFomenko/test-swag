424c474f0ef985e889c67f29a0b82c46e6630092
Vyacheslav Fomenko <playfog3@gmail.com>
2025-08-08 18:22:51 +0300

    add: test with wrong login and password

--- test/specs/login.e2e.ts
+++ test/specs/login.e2e.ts
@@ -1,17 +1,42 @@
 import {expect} from '@wdio/globals'
 import LoginPage from '../pageobjects/login.page'
 import InventoryPage from "../pageobjects/inventory.page";
-
+import {faker} from '@faker-js/faker';
 
 describe("Test Login flow", () => {
-    const loginPage = new LoginPage();
-    const inventoryPage = new InventoryPage();
-    it("should login with valid credentials", async () => {
-        await loginPage.open();
+    let loginPage: LoginPage;
+    let inventoryPage: InventoryPage;
 
-        await loginPage.login(process.env.USERNAME_LOGIN!, process.env.PASSWORD_LOGIN!); //JSON.stringify(process.env.USERNAME), JSON.stringify(process.env.PASSWORD)
-        await browser.pause();
-        expect(await inventoryPage.isDisplay());
+    beforeEach(async () => {
+        loginPage = new LoginPage();
+        inventoryPage = new InventoryPage();
+
+        await loginPage.open();
+    });
+
+    it("should login with valid credentials", async () => {
+        await loginPage.login(process.env.USERNAME_LOGIN || "standard_user", process.env.PASSWORD_LOGIN || "secret_sauce");
+        expect(await inventoryPage.isDisplay()).toBe(true);
+    });
+
+    it("should show error with invalid password", async () => {
+        const password = faker.internet.password();
+
+        await loginPage.login(process.env.USERNAME_LOGIN!, password); //JSON.stringify(process.env.USERNAME), JSON.stringify(process.env.PASSWORD)
+        await expect(loginPage.errorIcon).toBeDisplayed();
+        await expect(loginPage.errorText).toHaveText(
+            "Epic sadface: Username and password do not match any user in this service"
+        );
+    });
+
+    it("should show error with invalid login", async () => {
+        const login = faker.internet.username();
+
+        await loginPage.login(login, process.env.PASSWORD_LOGIN!); //JSON.stringify(process.env.USERNAME), JSON.stringify(process.env.PASSWORD)
+        await expect(loginPage.errorIcon).toBeDisplayed();
+        await expect(loginPage.errorText).toHaveText(
+            "Epic sadface: Username and password do not match any user in this service"
+        );
     });
 });
 
