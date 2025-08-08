3e741ed01da430b5bba80fb2e6732c4d0cf04330
Vyacheslav Fomenko <playfog3@gmail.com>
2025-08-08 19:09:31 +0300

    update: Inventory POM

--- test/pageobjects/inventory.page.ts
+++ test/pageobjects/inventory.page.ts
@@ -5,6 +5,24 @@ export default class InventoryPage {
         return $("#inventory_container");
     }
 
+    private get menuButton(): ChainablePromiseElement {
+        return $("#react-burger-menu-btn")
+    }
+
+    private get logoutBtn(): ChainablePromiseElement {
+        return $("#logout_sidebar_link")
+    }
+
+    public async openMenu(): Promise<void> {
+        await this.menuButton.waitForClickable({ timeout: 5000 });
+        await this.menuButton.click();
+    }
+
+    public async logout(): Promise<void> {
+        await this.logoutBtn.waitForClickable({ timeout: 5000 });
+        await this.logoutBtn.click();
+    }
+
     public async isDisplay(): Promise<boolean> {
         return this.inventoryContainer.isDisplayed();
     }
