7ee2104be5120cfb2440bc0619dca1b1af15ed31
Vyacheslav Fomenko <playfog3@gmail.com>
2025-08-09 15:51:28 +0300

    add: methods for price check

--- test/pageobjects/card.page.ts
+++ test/pageobjects/card.page.ts
@@ -1,4 +1,9 @@
 export default class CardPage {
+    public get container() { return $('#cart_contents_container'); }
+    public get checkoutBtn() { return $('[data-test="checkout"]'); }
+
+    async assertLoaded() { await expect(this.container).toBeDisplayed(); }
+
     public get itemName() {
         return $$('//div[contains(@class,"cart_item")]//div[contains(@class,"inventory_item_name")]');
     }
@@ -11,4 +16,18 @@ export default class CardPage {
         }
         return texts;
     }
+
+    async itemPrices(): Promise<number[]> {
+        const texts = await $$('[data-test="inventory-item-price"]').map(el => el.getText());
+        return texts.map(money);
+    }
+
+    async itemSubtotal(): Promise<number> {
+        const text = await $('[data-test="subtotal-label"]').getText(); // "Item total: $39.98"
+        return money(text);
+    }
+
+    async checkout() { await this.checkoutBtn.click(); }
 }
+
+const money = (s: string) => Number(s.replace(/[^\d.]/g, ''));
